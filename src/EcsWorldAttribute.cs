411429e277d14c75e8ec0dabc5269eeffcc42e78
Nikolay Plekhanov <kkolyankkolyan@gmail.com>
2021-12-10 19:21:46 +0300

    attributes extracted to its files

---
+++ src/EcsWorldAttribute.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Kk.BusyEcs
+{
+    [AttributeUsage(AttributeTargets.Struct)]
+    public class EcsWorldAttribute : Attribute
+    {
+        public readonly string name;
+
+        public EcsWorldAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+}
